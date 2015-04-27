295cbcc418c4867d4b50afeb0ff76795aa12c9ca
Ian MacDonald & Kevin Chen <student@epicodus.com>
2015-04-27 15:06:43 -0700

    Add css to pages.

--- app.rb
+++ app.rb
@@ -12,15 +12,15 @@ get('/triangles') do
   side_2 = params.fetch('side_2').to_i
   side_3 = params.fetch('side_3').to_i
   @result = ""
-  triangle = Triangle.new(side_1, side_2, side_3)
-  if triangle.isosceles?()
+  @triangle = Triangle.new(side_1, side_2, side_3)
+  if @triangle.isosceles?()
     @result = "isosceles"
-  elsif triangle.equilateral?()
+  elsif @triangle.equilateral?()
     @result = "equilateral"
-  elsif triangle.scalene?()
+  elsif @triangle.scalene?()
     @result = "scalene"
   else
-    @result = "Sorry, those numbers don't make a triangle"
+    @result = "not a real"
   end
   erb(:triangles)
 end
